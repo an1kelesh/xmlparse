531eb6eb42fcb974b375b8dd133264568fce63e5
an1kelesh <156405116+an1kelesh@users.noreply.github.com>
2024-04-18 10:35:22 +0300

    Add files via upload

---
+++ xmlparse.go
@@ -0,0 +1,38 @@
+package xmlparse
+
+import (
+	"encoding/xml"
+	"io"
+	"net/http"
+)
+
+type OvalDefinitions struct {
+	Definitions struct {
+		Definition []struct {
+			Metadata struct {
+				Title     string `xml:"title"`
+				Reference []struct {
+					RefID  string `xml:"ref_id,attr"`
+					RefURL string `xml:"ref_url,attr"`
+				} `xml:"reference"`
+				Description string `xml:"description"`
+				Advisory    struct {
+					Severity string `xml:"severity"`
+				} `xml:"advisory"`
+			} `xml:"metadata"`
+			Criteria struct {
+				Criterion struct {
+					Comment string `xml:"comment,attr"`
+				} `xml:"criterion"`
+			} `xml:"criteria"`
+		} `xml:"definition"`
+	} `xml:"definitions"`
+}
+
+func xmldownload(s OvalDefinitions) OvalDefinitions {
+	resp, _ := http.Get("https://redos.red-soft.ru/support/secure/redos.xml")
+	bytes, _ := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	xml.Unmarshal([]byte(bytes), &s)
+	return s
+}
